85d33094fca44f2e3bc6b8c3c74319b39b814fbc
Balazs Bamer <kolaloka@freemail.hu>
2019-04-22 21:50:13 +0200

    Added square wave velocity test app. Experimenting with PID controllers.

---
+++ src/cdpr_gazebo/src/squarevelocitytest.cpp
@@ -0,0 +1,35 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <cmath>
+
+int main(int argc, char **argv) {
+  const double cPublishFrequency  = 10.0; // Hz
+  const double cVelocityAmplitude =  0.5; // m/s
+  const double cVelocityFrequency =  0.2; // Hz
+  const size_t cWireCount         =  4u;
+
+  ros::init(argc, argv, "squarevelocitytest");
+  ros::NodeHandle nodeHandle;
+  ros::Publisher publisher = nodeHandle.advertise<sensor_msgs::Joy>("jointVelocities", 100);
+  ros::Rate loopRate(cPublishFrequency);
+
+  sensor_msgs::Joy velocityCommand;
+  velocityCommand.axes.resize(cWireCount);
+
+  double time = 0.0;
+  while (ros::ok()) {
+    double velocity = copysign(cVelocityAmplitude, sin(time * cVelocityFrequency * 2 * M_PI));
+    for(size_t i = 0; i < cWireCount; ++i) {
+      velocityCommand.axes[i] = velocity;
+    }
+    ROS_INFO("%f", velocity);
+
+    publisher.publish(velocityCommand);
+
+    ros::spinOnce();
+
+    loopRate.sleep();
+    time += 1.0 / cPublishFrequency;
+  }
+  return 0;
+}
