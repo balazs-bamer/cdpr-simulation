75c8bf8ab62cdff50d0a14412d8327b26930c5a7
Balazs Bamer <kolaloka@freemail.hu>
2019-06-16 16:23:44 +0200

    Checked tuning on higher velocity targets. Decreased position PID settling time.

--- src/cdpr_gazebo/src/sinevelocitytest.cpp
+++ src/cdpr_gazebo/src/sinevelocitytest.cpp
@@ -3,14 +3,27 @@
 #include <cmath>
 
 int main(int argc, char **argv) {
-  const double cGiga              = 1e9;
-  const double cPublishFrequency  = 10.0; // Hz
-  const double cVelocityAmplitude =  0.05; // m/s
-  const double cVelocityFrequency =  0.1; // Hz
-  const size_t cWireCount         =  4u;
+  const double cGiga              =   1e9;
+  const double cPublishFrequency  = 100.0; // Hz
+  const double cVelocityAmplitude =   0.05; // m/s
+  const double cVelocityFrequency =   0.1; // Hz
+  const size_t cWireCount         =   4u;
 
   ros::init(argc, argv, "sinevelocitytest");
   ros::NodeHandle nodeHandle;
+  ros::NodeHandle paramNodeHandle("~");
+  double velocityAmplitude;
+  double velocityFrequency;
+  if(!paramNodeHandle.getParam("freq", velocityFrequency)) {
+    velocityFrequency = cVelocityFrequency;
+  }
+  else { // nothing to do
+  }
+  if(!paramNodeHandle.getParam("amp", velocityAmplitude)) {
+    velocityAmplitude = cVelocityAmplitude;
+  }
+  else { // nothing to do
+  }
   ros::Publisher publisher = nodeHandle.advertise<sensor_msgs::Joy>("jointVelocities", 100);
   ros::Rate loopRate(cPublishFrequency);
 
@@ -19,7 +32,7 @@ int main(int argc, char **argv) {
 
   double time = 0.0;
   while (ros::ok()) {
-    double velocity = cVelocityAmplitude * sin(time * cVelocityFrequency * 2 * M_PI);
+    double velocity = velocityAmplitude * sin(time * velocityFrequency * 2 * M_PI);
     for(size_t i = 0; i < cWireCount; ++i) {
       velocityCommand.axes[i] = velocity;
     }
